e91d93700c19386a411f4967b3d3fbed543e634a
DiegoZunigT <163076241+DiegoZunigT@users.noreply.github.com>
2024-04-23 17:17:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,10 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-
-
+  unsigned long capacity = map->capacity;
+  unsigned long pos = hash(key, capacity);
+  map->buckets[pos]->value = value;
+  //map->current = map->buckets[pos]->value;
 }
 
 void enlarge(HashMap * map) {

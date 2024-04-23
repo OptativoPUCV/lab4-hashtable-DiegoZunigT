65aa3929a5cf21e2f8fdd5e1704f6148c19980bf
DiegoZunigT <163076241+DiegoZunigT@users.noreply.github.com>
2024-04-23 16:59:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,7 +54,8 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) 
 {
   HashMap *map = (HashMap *) malloc (sizeof(HashMap));
-  for(long i = 0; i <= capacity; i++) {
+  for(long i = 0; i < capacity; i++) {
+    map->buckets[i] = NULL;
   }
   return map;
 }

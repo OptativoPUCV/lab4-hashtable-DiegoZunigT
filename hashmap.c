0dfff8a612f6bb7340e385cb1ee048630adedb44
DiegoZunigT <163076241+DiegoZunigT@users.noreply.github.com>
2024-04-24 04:09:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -91,8 +91,12 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
-  
-  return NULL;
+  long pos = 0;
+  while(map->buckets[pos] == NULL) {
+    pos = (pos + 1) % map->capacity;
+  }
+  map->current = pos;
+  return map->buckets[pos];
 }
 
 Pair * nextMap(HashMap * map) {

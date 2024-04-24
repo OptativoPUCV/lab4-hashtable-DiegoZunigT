6081289ad5ed66cbac57fa38fea1a77b80f027c1
DiegoZunigT <163076241+DiegoZunigT@users.noreply.github.com>
2024-04-24 03:58:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -79,7 +79,10 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) 
 {
   long pos = hash(key, map->capacity);
-  if(map->buckets[pos] != NULL) {
+  if(map->buckets[pos]->key == key) {
+    map->current = pos;
+    return map->buckets[pos];
+  } else if(map->buckets[pos] != NULL) {
     while(map->buckets[pos] != NULL) {
       pos = (pos + 1) % map->capacity;
     }
